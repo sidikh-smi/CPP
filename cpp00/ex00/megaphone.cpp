af43b439991cab5d3403e7dd4df32021897e4b5b
sidikh-smi <skhaliff@student.1337.ma>
2023-04-17 00:07:14 +0000

    cpp00-ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    int i = 1;
+    char    uppercase;
+    if (argc >= 2)
+    {
+        while (argv[i])
+        {
+            int j = 0;
+            while (argv[i][j])
+            {
+                uppercase = toupper(argv[i][j]);
+                std :: cout << uppercase;
+                j++;
+            }
+            std::cout << " ";
+            i++;
+        }
+        std::cout << "\n";
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
