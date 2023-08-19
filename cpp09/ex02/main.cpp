a8f215edd2988c22be5f2df941c9969128dca978
Sidi Khaliffa <skhaliff@c3r7p10.1337.ma>
2023-08-19 20:57:21 +0100

    ...

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
             vector.push_back(a);
         i++;
     }
-    std::cout << vector.back() << std::endl;
+    // std::cout << vector.back() << std::endl;
     std::vector<std::pair<unsigned int, unsigned int > > pairv;
     size_t j = 0;
     while(j < vector.size())
@@ -36,7 +36,32 @@ int main(int argc, char **argv)
         if(p.first > p.second)
             std::swap(p.first, p.second);
         pairv.push_back(p);
-        vector.erase(vector.begin(), vector.begin() + 1);
         j += 2;
+    }
+    vector.clear();
+    std::vector<unsigned int> smalest;
+    for(std::vector<std::pair<unsigned int, unsigned int> >::iterator it = pairv.begin(); it < pairv.end(); it++)
+    {
+        unsigned int a;
+        a = it->first;
+        smalest.push_back(a);
+    }
+    std::vector<unsigned int> largest;
+    for(std::vector<std::pair<unsigned int, unsigned int> >::iterator it = pairv.begin(); it < pairv.end(); it++)
+    {
+        unsigned int b;
+        b = it->second;
+        largest.push_back(b);
+    }
+    std::sort(smalest.begin(), smalest.end());
+    std::vector<unsigned int>::iterator q;
+    for (std::vector<unsigned int>::iterator it = largest.begin(); it < largest.end(); it++)
+    { 
+        q = std::lower_bound(smalest.begin(), smalest.end(), *it);
+        smalest.insert(q, *it);
+    }
+    for (std::vector<unsigned int>::iterator it = smalest.begin(); it < smalest.end(); it++)
+    {
+        std::cout << *it << std::endl;
     }
 }
